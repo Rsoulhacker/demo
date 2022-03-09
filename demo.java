c54d73eaf6b7b081fe5310bd42d7e331580bc71b
sachin <sachinsachhu49@gmail.com>
2022-03-09 12:28:56 +0530

    first commit

--- demo.java
+++ demo.java
@@ -1,5 +1,9 @@
 public class demo{
 public static void main(String args[]){
 System.out.println("Hello world");
+System.out.println("Hello world");
+System.out.println("Hello world");
+System.out.println("Hello world");
+System.out.println("Hello world");
 }
 }
